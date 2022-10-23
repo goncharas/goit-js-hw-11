e1f4fa83d3707f3425575bdfed28bf84b1d9146b
Andrii Honchar <goncharas1988@gmail.com>
2022-10-23 22:09:30 +0300

    finish commit

--- src/js/search-images/fetch-images.js
+++ src/js/search-images/fetch-images.js
@@ -1,8 +1,9 @@
-import axios from "axios";
-export default async function (value, page) {
-    const url = 'https://pixabay.com/api/';
-    const key = '30756391-9d368ba7c089b21e6ce38e5cc';
-    const filter = `?key=${key}&q=${value}&image_type=photo&orientation=horizontal&safesearch=true&per_page=408page=${page}`;
+import axios from 'axios';
 
-    return await axios.get(`${url}${filter}`).then(response => response.data);
+export default async function fetchImages(value, page) {
+  const url = 'https://pixabay.com/api/';
+  const key = '30756391-9d368ba7c089b21e6ce38e5cc';
+  const filter = `?key=${key}&q=${value}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`;
+
+  return await axios.get(`${url}${filter}`).then(response => response.data);
 }
